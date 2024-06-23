f1536fa60be62480fb5c03db9d10c5f04addf901
sai shravan <saishravan384@gmail.com>
2024-06-23 13:50:48 +0530

    feat: Fetch data from the api to show the blog posts and comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,24 +1,54 @@
 import '../styles/components/Comment.css'
+import PropTypes from 'prop-types'
 
-export default function Comment() {
+Comment.propTypes = {
+  content: PropTypes.string,
+  author: PropTypes.object,
+  updatedAt: PropTypes.string,
+}
+
+export default function Comment({ content, author, updatedAt }) {
+  const getDateAgo = (date) => {
+    const now = new Date()
+    const diff = now - date
+
+    // Extract units
+    const seconds = Math.floor(diff / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
+    const days = Math.floor(hours / 24)
+    const weeks = Math.floor(days / 7)
+
+    // Format the string based on the difference
+    if (seconds < 60) {
+      return 'now'
+    } else if (minutes < 60) {
+      return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`
+    } else if (hours < 24) {
+      return `${hours} hour${hours !== 1 ? 's' : ''} ago`
+    } else if (days < 7) {
+      return `${days} day${days !== 1 ? 's' : ''} ago`
+    } else {
+      return `${weeks} week${weeks !== 1 ? 's' : ''} ago`
+    }
+  }
+
   return (
     <div className='comment'>
       <div className='author'>
         <div className='profilePicContainer'>
           <img
-            src='https://web.dev/static/blog/web-platform-05-2024/hero.png'
-            alt='card cover'
+            src={
+              author.profilePicUrl ||
+              'https://res.cloudinary.com/dmt9s5xlh/image/upload/v1719130473/defaultProfile_vgn5sr.png'
+            }
+            alt={'comment author'}
           />
         </div>
-        <p className='username'>John Doe</p>
-        <p className='time'>3 days ago</p>
+        <p className='username'>{author.firstName + ' ' + author.lastName}</p>
+        <p className='time'>{getDateAgo(new Date(updatedAt))}</p>
       </div>
-      <p className='content'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste fugit
-        adipisci, iusto distinctio mollitia laboriosam officiis ipsam, possimus
-        commodi vero voluptas fugiat dolorem harum consequuntur quidem dolores
-        libero? Assumenda, fuga!
-      </p>
+      <p className='content'>{content}</p>
     </div>
   )
 }
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,82 +1,68 @@
 import '../styles/pages/Post.css'
 import Comment from '../components/Comment'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 export default function Post() {
+  const [post, setPost] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true)
+        const res = await axios.get(`http://localhost:3000/posts/${id}`)
+        setPost(res.data)
+      } catch (error) {
+        setError(true)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [id])
+
   const handleAddCommentClick = () => {
     console.log('display the dialog')
+  }
+
+  if (isLoading) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    )
+  }
+
+  if (error) {
+    return (
+      <>
+        <p>An error occured while fetching the post!</p>
+        <p>{error}</p>
+      </>
+    )
   }
 
   return (
     <main id='post'>
       <div className='coverImageContainer'>
         <img
-          src='https://web.dev/static/blog/web-platform-05-2024/hero.png'
-          alt='card cover'
+          src={
+            post.coverImgUrl ||
+            'https://web.dev/static/blog/web-platform-05-2024/hero.png'
+          }
+          alt={post.title}
         />
       </div>
-      <h1>Post heading</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-        quibusdam commodi nostrum odio provident ea maiores dolorum, sunt
-        pariatur, quod similique tempore in ipsum voluptas nam. Repellendus
-        porro dolorum expedita rem dicta! Iusto amet, possimus saepe enim ipsa
-        rerum magnam repellat porro odit odio esse fugit deleniti. Excepturi
-        fugit incidunt fuga ratione? Temporibus reprehenderit amet, quasi
-        corrupti dolorem sit vitae soluta neque doloribus illum dicta non
-        distinctio saepe error ab officiis totam molestiae odio? Magnam magni
-        quas eius fuga eligendi quisquam aperiam saepe voluptates ducimus. Eos
-        consequatur delectus eveniet sit expedita rerum ad, excepturi quaerat
-        porro veritatis vero saepe doloribus distinctio sequi ipsum aspernatur
-        dolorem alias, mollitia consequuntur quos quod, nobis vel. Officia sunt
-        sed ratione fuga porro odio eius quis illum consequatur praesentium
-        aspernatur commodi laudantium repudiandae quae, tempora optio quam
-        incidunt amet perspiciatis. Vel minima delectus suscipit esse nisi totam
-        incidunt quos voluptatum autem placeat deleniti ut, fugiat nihil
-        mollitia maxime cupiditate sequi atque ad? Architecto obcaecati quasi
-        laborum, sequi voluptas voluptatibus aliquid rerum ullam. Eaque
-        accusantium repudiandae aperiam repellat delectus optio pariatur
-        corporis. Soluta maxime, eius minima obcaecati iure quisquam doloremque
-        illo fugiat asperiores illum nemo perferendis sit ratione temporibus
-        accusamus recusandae quae maiores expedita corporis dolorum laborum id
-        magnam in velit. Ut, dolor voluptate officia quod asperiores
-        consequuntur dicta nihil sequi, omnis nemo expedita dolores magni
-        tempore a quae praesentium culpa ipsam quos at esse velit repudiandae
-        beatae quidem labore. Nulla cupiditate porro a cumque tempora
-        repellendus temporibus itaque rerum, cum corporis et quidem odit, sunt
-        doloremque, quos ab saepe eius corrupti at quia debitis! Quam fugiat
-        laborum at saepe animi, quaerat mollitia praesentium molestias, harum
-        atque numquam. Nesciunt natus maxime eveniet ipsam, saepe fugit optio
-        adipisci repellendus! Accusantium labore similique vero minus vel ea cum
-        facilis reprehenderit sint alias esse ullam repellendus expedita in
-        soluta fugit rerum fuga quo maiores culpa, ut rem? Assumenda odit soluta
-        consequuntur saepe non suscipit adipisci, hic minima neque laborum
-        eaque. Provident, at nemo dignissimos assumenda explicabo in
-        consequuntur ducimus sit, hic ex blanditiis recusandae! Optio laboriosam
-        ex obcaecati? Ex, sint eligendi, labore sequi ut molestiae neque maiores
-        quo necessitatibus, voluptate aperiam maxime ipsum deleniti voluptatem
-        aliquid voluptatum id. Quos ipsum eaque iure quidem nemo, sapiente,
-        adipisci cumque dicta commodi officiis tempora quas modi. Reprehenderit
-        fugiat soluta aliquid alias deserunt nostrum neque nobis dicta
-        explicabo? Nobis architecto vero eos. Non amet adipisci aliquam, sit a
-        odit laudantium, placeat accusantium nobis saepe impedit quasi vel minus
-        modi suscipit tenetur magnam aliquid recusandae laborum? Provident,
-        atque vero. Excepturi hic id voluptatibus ullam libero doloribus nisi a
-        maiores provident, ad cumque velit nostrum consequatur. Corporis sint
-        dicta cupiditate quam eos nisi tempora odit minima ipsum totam officiis
-        suscipit, in omnis ab magnam officia quos ratione velit hic. Dolorum,
-        velit quo. Quo odit facilis in odio, vel fugiat adipisci, alias
-        asperiores necessitatibus nobis tenetur eaque, maiores quam. Enim
-        nostrum officiis accusamus, molestias tempore quas labore aperiam! Odit
-        id, recusandae labore hic iure ex harum iste minima itaque repellat esse
-        laborum, mollitia qui culpa dolore?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-        laudantium odio nisi sed animi quibusdam, modi consequuntur cum facere
-        atque. Voluptatibus id quos nesciunt itaque neque consequuntur debitis
-        eos repellat?
-      </p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+
       <h2>Comments</h2>
+
       <div className='comments'>
         {/* todo: display a model on this button click */}
         <button className='filled addComment' onClick={handleAddCommentClick}>
@@ -90,8 +76,19 @@ export default function Post() {
           </svg>
           <span>Add comment</span>
         </button>
-        <Comment />
-        <Comment />
+
+        {post.comments.length === 0 && (
+          <p>There are no comments on this post.</p>
+        )}
+        {post.comments.length > 0 &&
+          post.comments.map((comment) => (
+            <Comment
+              key={comment._id}
+              content={comment.content}
+              author={comment.author}
+              updatedAt={comment.updatedAt}
+            />
+          ))}
       </div>
     </main>
   )
